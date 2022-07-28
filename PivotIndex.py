8ad3903084cb5eb1ce44480cd4327029f1e0cd6e
YusufHasan007 <62721671+YusufHasan007@users.noreply.github.com>
2022-07-29 00:13:05 +0530

    Create PivotIndex.py

---
+++ PivotIndex.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        arrSum = sum(nums) 
+        rightsum = 0
+        leftsum = 0
+        
+        for i in range(len(nums)):
+            
+            rightsum = arrSum - nums[i] - leftsum
+            
+            if rightsum == leftsum:
+                return i
+            
+            leftsum += nums[i]
+        return -1
