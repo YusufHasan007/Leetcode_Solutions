b9fdb3bdf3cbe103a3954b08d40c290ea4393a87
YusufHasan007 <62721671+YusufHasan007@users.noreply.github.com>
2022-07-29 00:12:25 +0530

    Create RunningSumOf1Darray.py

---
+++ RunningSumOf1Darray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        count = 0
+        l = []
+
+        for i in range(len(nums)):
+            count += nums[i]
+            l.append(count)
+            
+        return l  
