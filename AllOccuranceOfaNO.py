3db9f3083d61b1be420826b587a1e92079582e0b
Yusuf Hasan <62721671+YusufHasan007@users.noreply.github.com>
2022-07-29 18:44:11 +0530

    Update AllOccuranceOfaNO.py

--- AllOccuranceOfaNO.py
+++ AllOccuranceOfaNO.py
@@ -5,3 +5,20 @@ class Solution:
         out = [i for i,x in enumerate(sorted(nums)) if x == target]
         
         return out
+    
+    
+************************************************************************************
+
+# ***Alternate Solution***
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        
+        nums.sort()
+        out = []
+        
+        for i in range(len(nums)):
+            if nums[i] == target:
+                out.append(i)
+                
+        return out       
