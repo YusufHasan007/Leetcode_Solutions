2c587083f01d37c2ddc23465b7571770ec2a72fe
Yusuf Hasan <62721671+yh-Yusuf@users.noreply.github.com>
2022-08-30 00:21:51 +0530

    Update AllOccuranceOfaNO.py

--- AllOccuranceOfaNO.py
+++ AllOccuranceOfaNO.py
@@ -1,7 +1,6 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        
-      
+       
         out = [i for i,x in enumerate(sorted(nums)) if x == target]
         
         return out
